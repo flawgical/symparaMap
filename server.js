1c16460dda9293f997579a6de8b733bec02736b8
Robin <daorob@gmail.com>
2020-03-30 23:48:00 -0500

    Repurpose users resource for api resource instead. GET route responds successfully.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const methodOverride = require('method-override');
 require('./config/database');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
