5b7628d7531b276566129ff8a4a417765674911f
Robin <daorob@gmail.com>
2020-03-31 01:21:51 -0500

    Corrected api route comment.

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const mapsCtrl = require('../controllers/maps');
 
-/* GET maps listing. */
+/* GET /api/maps listing. */
 router.get('/maps', mapsCtrl.index);
 router.post('/maps', mapsCtrl.create);
 router.get('/maps/:id', mapsCtrl.show);
